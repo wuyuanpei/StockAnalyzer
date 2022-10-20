588a96fa6fca3a60db8ae8a939d31ad406b1b671
wuyuanpei <yuanpei.wu.0219@gmail.com>
2022-10-20 14:36:25 +0800

    output cleanup

--- daily_rsi_strategy_select_tencent.py
+++ daily_rsi_strategy_select_tencent.py
@@ -10,6 +10,7 @@ with open(stocks_file) as f:
 
 buy_list_last_day = []
 buy_list_today = []
+hold_list = []
 
 for stockname in stock_names:
     df_price = get_df_price(stockname)
@@ -24,10 +25,16 @@ for stockname in stock_names:
         continue
 
     if rsi_signal[-2] == 1:
-        buy_list_last_day.append(stockname)
+        buy_list_last_day.append((stockname, buy_price[-2]))
 
     if rsi_signal[-1] == 1:
-        buy_list_today.append(stockname)
+        buy_list_today.append((stockname, buy_price[-1]))
+
+    for i in range(len(rsi_signal)):
+        if rsi_signal[len(rsi_signal) - 1 -i] == -1 or i > 10:
+            break
+        elif rsi_signal[len(rsi_signal) - 1 -i] == 1:
+            hold_list.append((stockname, i, df_price['closeprice'][len(rsi_signal) - 1 -i], df_price['closeprice'][len(rsi_signal) - 1]))
 
 buy_list_last_day_good = dict()
 if len(buy_list_last_day) > 0:
@@ -36,7 +43,7 @@ if len(buy_list_last_day) > 0:
         switch = False
         for line in f:
         
-            if line.find(buy_list_last_day[idx]) != -1:
+            if line.find(buy_list_last_day[idx][0]) != -1:
                 switch = True
 
             pos = line.find("总年化收益：")
@@ -49,9 +56,6 @@ if len(buy_list_last_day) > 0:
                 if idx >= len(buy_list_last_day):
                     break
 
-print("yesterday:{}".format(sorted(buy_list_last_day_good.items(),key=lambda s:s[1])))
-
-
 buy_list_today_good = dict()
 if len(buy_list_today) > 0:
     with open("results_tencent.txt") as f:
@@ -59,7 +63,7 @@ if len(buy_list_today) > 0:
         switch = False
         for line in f:
         
-            if line.find(buy_list_today[idx]) != -1:
+            if line.find(buy_list_today[idx][0]) != -1:
                 switch = True
 
             pos = line.find("总年化收益：")
@@ -72,4 +76,36 @@ if len(buy_list_today) > 0:
                 if idx >= len(buy_list_today):
                     break
 
-print("today:{}".format(sorted(buy_list_today_good.items(),key=lambda s:s[1])))
+
+
+hold_list_good = dict()
+if len(hold_list) > 0:
+    with open("results_tencent.txt") as f:
+        idx = 0
+        switch = False
+        for line in f:
+        
+            if line.find(hold_list[idx][0]) != -1:
+                switch = True
+
+            pos = line.find("总年化收益：")
+        
+            if switch and pos != -1:
+                hold_list_good[hold_list[idx]] = float(line[pos + 6:-2])
+                idx += 1
+                switch = False
+
+                if idx >= len(hold_list):
+                    break
+
+print("\n=================================== 10-day holding ===================================")
+for k, v in sorted(hold_list_good.items(),key=lambda s:-s[1]):
+    print("代码: {}\t\t历史收益: {}%\t\t持有天数: {}\t\t买价: {}\t\t现价: {}".format(k[0], v, k[1], k[2], k[3]))
+
+print("\n================================= yesterday buypoint =================================")
+for k, v in sorted(buy_list_last_day_good.items(),key=lambda s:-s[1]):
+    print("代码: {}\t\t历史收益: {}%\t\t买价: {}".format(k[0], v, k[1]))
+
+print("\n=================================== today buypoint ===================================")
+for k, v in sorted(buy_list_today_good.items(),key=lambda s:-s[1]):
+    print("代码: {}\t\t历史收益: {}%\t\t买价: {}".format(k[0], v, k[1]))
